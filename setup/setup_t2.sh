d1ae4e3711630fb67e7fbabe8b10f5b4c1dc9508
rajeshunnikrishnan <rajesh.ukrishnan@gmail.com>
2017-12-20 23:20:49 +0530

    Update setup_t2.sh

--- setup/setup_t2.sh
+++ setup/setup_t2.sh
@@ -5,7 +5,7 @@
 # get the correct ami
 export region=$(aws configure get region)
 if [ $region = "us-west-2" ]; then
-   export ami="ami-f8fd5998" # Oregon
+   export ami="ami-64c5cc1d" # Oregon
 elif [ $region = "eu-west-1" ]; then
    export ami="ami-9e1a35ed" # Ireland
 elif [ $region = "us-east-1" ]; then
@@ -15,6 +15,6 @@ else
   exit 1
 fi
 
-export instanceType="t2.xlarge"
+export instanceType="t2.micro"
 
 . $(dirname "$0")/setup_instance.sh
